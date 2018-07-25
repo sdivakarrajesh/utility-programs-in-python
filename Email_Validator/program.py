63a4253e5c2bd6067da9df2e4bb073d4d3709aa5
sdivakarrajesh <30803637+sdivakarrajesh@users.noreply.github.com>
2018-07-25 19:33:40 +0530

    added consumer email checker

--- Email_Validator/program.py
+++ Email_Validator/program.py
@@ -1,8 +1,32 @@
 import re
+consumerEmailProviders = ['gmail','ymail','yahoo','orkut','hotmail','aol','orange','live','rediffmail','outlook','googlemail','rockermail']
+def checkemail(str):
+	pattern = r"([\w\.\d-]+)@([\w\.=]+)(\.[\w\.]+)"
+	match = re.match(pattern,str)
+	return match
+def checkIfConsumerEmail(str):
+	startFrom = str.find('@')
+	endAt = str.rfind('.')
+	serviceProvider = str[startFrom+1:endAt]
+	if serviceProvider in consumerEmailProviders:
+		return True
+	else:
+		return False
+	
+
 str = input()
-pattern = r"([\w\.\d-]+)@([\w\.=]+)(\.[\w\.]+)"
-match = re.match(pattern,str)
+match = checkemail(str)
 if match:
 	print("Entered email is Valid")
+	match = checkIfConsumerEmail(str)
+	if match:
+		print("Entered email is a consumer email")
+	else:
+		print("Entered email is not a consumer email")
 else:
 	print("Entered email is not valid")
+
+
+	
+
+	
