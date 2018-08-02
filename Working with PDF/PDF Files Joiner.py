e11ad2996ed1435baf935f885e2551ce1a49baed
sdivakarrajesh <30803637+sdivakarrajesh@users.noreply.github.com>
2018-08-02 12:39:20 +0530

    added filename validation

--- Working with PDF/PDF Files Joiner.py
+++ Working with PDF/PDF Files Joiner.py
@@ -1,17 +1,27 @@
 import PyPDF2
+def nameFormat(name):
+	if len(name)>4:
+		if name[-1:-4:-1]!='.pdf':
+			name = name + '.pdf'
+	print(name)
+	return name
+
 count = int(input('Enter Number of PDFs to join:'))
 pdfWriter = PyPDF2.PdfFileWriter()
 finalName = input("Enter Final PDF Name:")
+finalName = nameFormat(finalName)
+pdfOutputFile = open(finalName,'wb')
 for i in range(count):
 	name = input("Enter File name:")
-	pdfFile1 = open(name,'rb')
-	pdfReader1 = PyPDF2.PdfFileReader(pdfFile1)
-	
-	for pageNum in range(pdfReader1.numPages):
-		pageObj = pdfReader1.getPage(pageNum)
+	name = nameFormat(name)
+	pdfFile = open(name,'rb')
+	pdfReader = PyPDF2.PdfFileReader(pdfFile)
+
+	for pageNum in range(pdfReader.numPages):
+		pageObj = pdfReader.getPage(pageNum)
 		pdfWriter.addPage(pageObj)
 
 	pdfOutputFile = open(finalName,'ab')
 	pdfWriter.write(pdfOutputFile)
-	pdfFile1.close()
-
+	pdfFile.close()
+pdfOutputFile.close()
