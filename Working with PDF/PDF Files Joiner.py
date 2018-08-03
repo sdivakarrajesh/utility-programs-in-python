d3c440b8ae088478b6f759fd5f8dd0579ed89e4c
sdivakarrajesh <30803637+sdivakarrajesh@users.noreply.github.com>
2018-08-03 22:24:59 +0530

    get proper filename

--- Working with PDF/PDF Files Joiner.py
+++ Working with PDF/PDF Files Joiner.py
@@ -1,27 +1,33 @@
 import PyPDF2
-def nameFormat(name):
-	if len(name)>4:
-		if name[-1:-4:-1]!='.pdf':
-			name = name + '.pdf'
-	print(name)
+def getProperFileName(strToPrint):
+	name = ''
+	while True:
+		name = input(strToPrint)
+		if len(name)>4:
+			if name[-1:-4:-1]!='.pdf':
+				name = name + '.pdf'
+		print(name)
+		nonAcceptedFileNameChar = ['/','\\',';','*','?','\"','<','>','|']
+		if any(letter in nonAcceptedFileNameChar for letter in name):
+			print("File Name cannot contain /\\;*?\"<>|")
+			continue
+		else:
+			break
 	return name
 
 count = int(input('Enter Number of PDFs to join:'))
 pdfWriter = PyPDF2.PdfFileWriter()
-finalName = input("Enter Final PDF Name:")
-finalName = nameFormat(finalName)
+finalName = getProperFileName("Enter Final PDF Name:")
 pdfOutputFile = open(finalName,'wb')
 for i in range(count):
-	name = input("Enter File name:")
-	name = nameFormat(name)
+	name = getProperFileName("Enter File name:")
 	pdfFile = open(name,'rb')
 	pdfReader = PyPDF2.PdfFileReader(pdfFile)
-
 	for pageNum in range(pdfReader.numPages):
 		pageObj = pdfReader.getPage(pageNum)
 		pdfWriter.addPage(pageObj)
-
 	pdfOutputFile = open(finalName,'ab')
 	pdfWriter.write(pdfOutputFile)
 	pdfFile.close()
+
 pdfOutputFile.close()
