d47e05ae8a46abd2a78ad83f8c6afe14da5c8052
sdivakarrajesh <30803637+sdivakarrajesh@users.noreply.github.com>
2018-08-07 22:54:07 +0530

    finding image resolution

---
+++ image resizer/image resizer.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+def getProperFileName(strToPrint):
+	name = ''
+	while True:
+		name = input(strToPrint)
+		nonAcceptedFileNameChar = ['/','\\',';','*','?','\"','<','>','|']
+		if any(letter in nonAcceptedFileNameChar for letter in name):
+			print("File Name cannot contain /\\;*?\"<>|.. Retry ")
+			continue
+		else:
+			break
+	return name
+
+def addExtension(name,extension):
+	if len(name)<5:
+		name = name + '.'+extension
+	if len(name)>4:
+	    if name[-1:-4:-1]!='.'+extension:
+		    name = name + '.'+extension
+	print(name)
+	return name
+
+extensions = ['png','jpg','gif']
+name = getProperFileName('Enter the name of the image file: ')
+found = False
+while True and len(extensions)>0:
+    nameWithExtension = addExtension(name,extensions[0])
+    try: imgFile = Image.open(nameWithExtension)
+    except FileNotFoundError: extensions.pop(0)
+    except: pass
+    else:
+        found = True
+        break
+
+if not found: print("You might have entered a wrong name. Try Again!")
+else: print(imgFile.size)
