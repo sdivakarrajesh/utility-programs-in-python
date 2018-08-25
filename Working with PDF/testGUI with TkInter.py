30de35f0457cb64376b972d3029042953f4c6e39
sdivakarrajesh <30803637+sdivakarrajesh@users.noreply.github.com>
2018-08-25 22:55:38 +0530

    added listBox and removed plain labels

--- Working with PDF/testGUI with TkInter.py
+++ Working with PDF/testGUI with TkInter.py
@@ -4,30 +4,41 @@ from tkinter.filedialog import askopenfilename,asksaveasfilename
 import PyPDF2
 from tkinter import messagebox
 class Application(Frame):
+
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.create_widgets()
         self.create_drop_files_here()
+        self.create_list_box()
+
+    def create_list_box(self):
+        self.lb = Listbox(width = 35)
+        Scrollbar(self.lb, orient="vertical")
+        Scrollbar(self.lb, orient="horizontal")
+        self.count = 1
+        self.lb.pack()
 
     def create_drop_files_here(self):
         self.entry_sv = StringVar()
         self.entry_sv.set('Drop Files Here...')
-        self.entry = Entry(root, textvar=self.entry_sv, width=80)
-        self.entry.pack(side="bottom", padx=10, pady=10)
+        self.entry = Entry(root, textvar=self.entry_sv, width=35)
+        self.entry.pack(side="bottom", padx=5, pady=5)
         self.entry.drop_target_register(DND_FILES)
         self.entry.dnd_bind('<<Drop>>', self.drop)
 
     def create_widgets(self):
         self.file_chooser = Button(self)
-        self.file_chooser["text"] = "Choose a File"
+        self.file_chooser["text"] = " Choose a File "
         self.file_chooser["command"] = self.add_fileChooser
-        self.file_chooser.pack(side="top")
+        label = Label(self,text="Choose PDFs to join: ",fg="black")
+        label.pack(side=LEFT,padx=5,pady=5)
+        self.file_chooser.pack(side=RIGHT,pady=5)
         self.file_list = []
-        self.joinFiles = Button(self, text="Join",
-                              command=self.joinPDFs)
-        self.joinFiles.pack(side="bottom")
-
+        frame = Frame(root)
+        frame.pack(side="bottom",pady=10)
+        self.joinFiles = Button(frame, text="   Join   ",command=self.joinPDFs)
+        self.joinFiles.pack(side=RIGHT)
 
     def add_fileChooser(self):
         Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
@@ -36,14 +47,8 @@ class Application(Frame):
         if filename!="" and filename!=None:
             self.file_list.append(filename)
             onlyName = filename.split("/")[-1]
-            frame = Frame(self)
-            self.label = Label(frame,text=onlyName, fg="black")
-            #self.label.pack(side=LEFT,fill=Y)
-            self.remove = Button(frame, text="remove", fg="red", command=self.remove_file)
-            #self.remove.pack(side=LEFT,fill=Y)
-            frame.pack()
-            self.label.pack(side=LEFT,fill=Y)
-            self.remove.pack(side=RIGHT,fill=Y)
+            self.count = self.count+1
+            self.lb.insert(self.count,onlyName)
 
     def remove_file(self):
         print("test")
@@ -51,20 +56,16 @@ class Application(Frame):
     def drop(self,event):
         self.returnedFilePaths = event.data
         self.filePaths()
+
     def filePaths(self):
         curr = self.returnedFilePaths[1:-1]
         list = curr.split("} {")
         self.file_list = self.file_list+list
         print(list)
-
         for i in list:
-            frame = Frame(self)
             onlyName = i.split("/")[-1]
-            label = Label(frame,text=onlyName,fg="black")
-            remove = Button(frame,text="remove",fg="red",command=self.remove_file)
-            frame.pack()
-            label.pack(side=LEFT,fill=Y)
-            remove.pack(side=RIGHT,fill=Y)
+            self.count = self.count+1
+            self.lb.insert(self.count,onlyName)
 
     def joinPDFs(self):
         try:
@@ -93,5 +94,5 @@ class Application(Frame):
 root = TkinterDnD.Tk()
 app = Application(master=root)
 app.master.title("My PDF joiner")
-app.master.geometry("250x250")
+app.master.geometry("300x300")
 app.mainloop()
